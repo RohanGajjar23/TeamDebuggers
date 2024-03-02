bcfd2289f41f121d17122f57ad598f94edc296af
HarshitaBhamare <harshitabhamare16@gmail.com>
2024-03-02 11:28:20 +0530

    rr

---
+++ lib/contoller/one.dart
@@ -0,0 +1,8 @@
+import 'package:get/get.dart';
+
+class HomeController extends GetxController {
+  // var x = ("").obs;
+  // RxString x = RxString("");
+  // RxList<String> x = RxList<String>([]);
+  Rx<List<String>> x = Rx<List<String>>([]);
+}
